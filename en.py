422f2f57b5b2d18d6f303c36633b6b4a4c33e8d0
r00t_k1n9 <46450756+soryecker@users.noreply.github.com>
2023-01-12 02:19:08 +0800

    源码加密文件

---
+++ en.py
@@ -0,0 +1,17 @@
+import ctypes
+
+po090o09o9o89iu9oi09ioi09o = print
+
+oo90o890i0oi9io9i0oi9k09ii = ctypes
+
+o090o09pi98io9i09i9ioi = open
+
+ok09i09i9io = ctypes.windll.kernel32.RtlMoveMemory
+
+o0oo00o0o0i09i09i = ctypes.c_uint64
+
+sg1r5g1rgb1seth4ckbu77er5he4zt51hs5 = ctypes.windll.kernel32.CreateThread
+i90i09i09i0i9 = ctypes.windll
+i9u89u98u98u0u09u0u990 = i90i09i09i0i9.kernel32.RtlMoveMemory
+
+fja8g9u9guare8g0argua0rgu = ctypes.c_uint64
